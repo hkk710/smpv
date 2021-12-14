bd7fde1a7dc57be045b2b25a01fa80fde799594a
Harikrishna A J <hari@renfos.com>
2021-12-14 23:07:39 +0530

    email functionality

--- app/Http/Controllers/CareerResourceController.php
+++ app/Http/Controllers/CareerResourceController.php
@@ -35,7 +35,7 @@ class CareerResourceController extends Controller
      */
     public function store(Request $request)
     {
-        dd("here");
+        // dd("here");
         $applicantDetails = $request->all();
         // if ($file = $request->file('file')) {
             // if ($file->move('pdf', $applicantDetails['email'] . 'pdf')) {
